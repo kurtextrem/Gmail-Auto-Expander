a8eadd851aa4257d8bbe77c5838d85ab06b78ddd
Jacob Groß <kurtextrem@gmail.com>
2017-09-19 15:02:32 +0200

    Switch to uglify-es

--- build.js
+++ build.js
@@ -3,30 +3,32 @@ var shell = require('shelljs'),
 	zipdir = require('zip-dir'),
 	fs = require('fs')
 
-(function () {
+;(function() {
 	'use strict'
 
-	var Build = function () {
+	var Build = function() {
 		this.args = process.argv.slice(2)
-		this.args.forEach(function (arg) {
-			if (this[arg]) {
-				this[arg]()
-			} else {
-				console.warn('undefined option', arg)
-			}
-		}.bind(this))
+		this.args.forEach(
+			function(arg) {
+				if (this[arg]) {
+					this[arg]()
+				} else {
+					console.warn('undefined option', arg)
+				}
+			}.bind(this)
+		)
 	}
 
-	Build.prototype.copy = function () {
+	Build.prototype.copy = function() {
 		this.copyLocales()
 		this.copyImg()
 	}
 
-	Build.prototype.copyLocales = function () {
-		fs.readdir('src/_locales', function (err, folder) {
+	Build.prototype.copyLocales = function() {
+		fs.readdir('src/_locales', function(err, folder) {
 			if (err) throw err
 
-			folder.forEach(function (name) {
+			folder.forEach(function(name) {
 				if (name !== '.' && name !== '..') {
 					shell.mkdir('-p', 'dist/_locales/' + name)
 					shell.exec('json-minify src/_locales/' + name + '/messages.json > dist/_locales/' + name + '/messages.json')
@@ -35,20 +37,19 @@ var shell = require('shelljs'),
 		})
 	}
 
-	Build.prototype.copyImg = function () {
+	Build.prototype.copyImg = function() {
 		shell.cp('src/*.png', 'dist')
 	}
 
-	Build.prototype.replaceJSON = function () {
+	Build.prototype.replaceJSON = function() {
 		shell.sed('-i', 'contentscript.js', 'contentscript.min.js', 'dist/manifest.json')
 	}
 
-	Build.prototype.buildZip = function () {
-		zipdir('dist', { saveTo: 'dist.zip' }, function (err) {
-			if (err)
-				console.error(err)
+	Build.prototype.buildZip = function() {
+		zipdir('dist', { saveTo: 'dist.zip' }, function(err) {
+			if (err) console.error(err)
 		})
 	}
 
 	new Build()
-}());
+})()
