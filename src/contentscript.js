11c3a74f99341724fe0af5850da2b339c47ca120
Jacob Groß <kurtextrem@gmail.com>
2017-09-20 09:11:16 +0200

    Fix type error

--- src/contentscript.js
+++ src/contentscript.js
@@ -11,45 +11,6 @@
 	}
 
 	/**
-	 * Handles the expansion of the mail.
-	 *
-	 * @author 	Jacob Groß
-	 * @date   	2016-09-09
-	 */
-	const label = /#label\/.+\/.+/
-	const inbox = /#(inbox|imp|all)\/.+/
-	function init() {
-		const hash = location.hash
-		if (!label.test(hash) && !inbox.test(hash)) return
-
-		let vem = document.getElementsByClassName('vem')[0]
-		if (vem !== undefined) {
-			let a = document.createElement('a')
-
-			a.href = 'javascript:null'
-			a.textContent = messages.expanding + '... (' + messages.clickHere + ')'
-			a.style.paddingLeft = '5px'
-			a.addEventListener('click', fetch.bind(this, vem.href), false)
-			vem.parentElement.appendChild(a)
-
-			fetch(vem.href)
-				.then(function(xhr) {
-					if (location.hash !== hash) return // issue #1
-
-					var elem = xhr.responseXML.querySelector('.message div > font')
-					if (!elem.innerText) return console.warn('empty message', elem.childNodes)
-
-					vem.parentElement.innerHTML = elem.innerHTML // swap content (.a3s)
-					hash = a = vem = null // prevent memory leak
-				})
-				.catch(function(error) {
-					a.textContent += ' ― ' + chrome.i18n.getMessage('error') + ' (' + messages.clickHere + ')'
-					console.error(error)
-				})
-		}
-	}
-
-	/**
 	 * Fetches the given URL; we can't use Fetch here, because it has no support for returning XML/HTML as response.
 	 *
 	 * @author 	Jacob Groß
@@ -59,7 +20,7 @@
 	 */
 	function fetch(href) {
 		return new Promise(function(resolve, reject) {
-			var xhr = new XMLHttpRequest()
+			const xhr = new XMLHttpRequest()
 			xhr.responseType = 'document'
 			xhr.onreadystatechange = function() {
 				if (xhr.readyState === 4) {
@@ -71,6 +32,51 @@
 			xhr.open('GET', href, true)
 			xhr.send()
 		})
+	}
+
+	/**
+	 * Handles the expansion of the mail.
+	 *
+	 * @author 	Jacob Groß
+	 * @date   	2016-09-09
+	 */
+	const label = /#label\/.+\/.+/
+	const inbox = /#(inbox|imp|all)\/.+/
+	function init() {
+		let hash = location.hash
+		if (!label.test(hash) && !inbox.test(hash)) return
+
+		let vem = document.getElementsByClassName('vem')[0]
+		if (vem !== undefined) {
+			let a = document.createElement('a')
+
+			a.href = 'javascript:null'
+			a.textContent = messages.expanding + '... (' + messages.clickHere + ')'
+			a.style.paddingLeft = '5px'
+			a.addEventListener('click', fetch.bind(undefined, vem.href), false)
+			vem.parentElement.appendChild(a)
+
+			fetch(vem.href)
+				.then(function(xhr) {
+					if (location.hash !== hash) return // issue #1
+
+					let elem = xhr.responseXML.querySelector('.message div > font')
+					if (!elem.innerText) {
+						console.warn(elem.childNodes)
+						throw new Error('empty message')
+					}
+
+					vem.parentElement.innerHTML = elem.innerHTML // swap content (.a3s)
+					hash = a = vem = elem = null // prevent memory leak
+
+					return xhr
+				})
+				.catch(function(error) {
+					a.textContent += ' ― ' + chrome.i18n.getMessage('error') + ' (' + messages.clickHere + ')'
+					console.error(error)
+					hash = a = vem = null // prevent memory leak
+				})
+		}
 	}
 
 	/**
