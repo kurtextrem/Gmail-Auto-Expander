70325c41e87cb11cf3543fd12e7102b26dfb04f2
Jacob Groß <kurtextrem@gmail.com>
2018-09-27 09:17:53 +0200

    Throttle functions

--- src/contentscript.js
+++ src/contentscript.js
@@ -38,19 +38,11 @@
 	const label = /#label(?:\/.+){2}/
 	const inbox = /#(inbox|imp|al{2}|search|trash|sent)\/.+/
 	const hrefToHash = new Map()
-	function hashChange() {
-		let hash = location.hash
-		if (!label.test(hash) && !inbox.test(hash)) return
 
-		let vem = document.getElementsByClassName('vem')
-		if (vem.length === 0) vem = document.querySelectorAll('.ii.gt > div > div > br + br + a')
-		const len = vem.length
+	const fetchFromElemThrottled = throttle(fetchFromElem, 250)
 
-		if (len === 0) return
-
-		let target
-		if (len !== 1) for (let i = 0; i < len; ++i) if (document.body.contains(vem[i])) target = vem[i]
-
+	function fetchFromElem(target) {
+		let hash = document.location.hash
 		let a = document.createElement('a')
 
 		const href = target.href,
@@ -60,25 +52,22 @@
 		hrefToHash.set(href, hash)
 		fetch(href)
 			.then(xhr => {
-				if (hrefToHash.get(href) !== location.hash) return hrefToHash.delete(href) // issue #1
+				if (hrefToHash.get(href) !== location.hash) return hrefToHash.delete(href) // If the XHR is returned to late, abort; issue #1
 
-				console.log(target, xhr.responseXML)
+				console.log(xhr.responseXML)
 				let elem = xhr.responseXML.querySelector('.message div > font')
 				if (!elem.textContent) {
 					console.warn(elem.childNodes)
 					throw new Error('empty message')
 				}
 
-				window.requestAnimationFrame(() => {
-					let $$ = document.querySelectorAll('.a3s'),
-						$len = $$.length
-					console.log($$)
-					$$[$len > 1 ? $len - 2 : $len - 1].innerHTML = elem.innerHTML // swap content
-					$$ = $len = elem = null
-				})
+				let $$ = document.querySelectorAll('.a3s'),
+					$len = $$.length
+				console.log($$)
+				$$[$len > 1 ? $len - 2 : $len - 1].innerHTML = elem.innerHTML // swap content
+				$$ = $len = elem = null
 
-				vem.length = 0
-				a = hash = vem = target = null // prevent memory leak
+				a = hash = null // prevent memory leak
 
 				return xhr
 			})
@@ -86,38 +75,73 @@
 				console.error(error)
 				a.textContent += ` ― ${chrome.i18n.getMessage('error')} (${chrome.i18n.getMessage('clickHere')})`
 
-				vem.length = 0
-				a = hash = vem = target = null // prevent memory leak
+				a = hash = null // prevent memory leak
 				return error
 			})
 
 		a.href = '#'
 		a.textContent = `${chrome.i18n.getMessage('expanding')}... (${chrome.i18n.getMessage('clickHere')})`
 		a.style.paddingLeft = '5px'
-		a.addEventListener('click', e => hrefToHash.delete(href) && e.preventDefault() && hashChange(), false)
+		a.addEventListener(
+			'click',
+			e => {
+				e.preventDefault()
+				hrefToHash.delete(href)
+				fetchFromElem(target)
+			},
+			false
+		)
 		target.parentElement.appendChild(a)
 	}
 
 	let observer
 	function observe() {
-		observer = new MutationObserver(function(mutations) {
-			const hash = document.location.hash
-			if (!label.test(hash) && !inbox.test(hash)) return
+		const div = document.querySelector('div[id=":5"] + div')
+		if (div === null) return
 
-			for (let i = 0; i < mutations.length; ++i) {
-				let mutation = mutations[i].target.querySelector('vem')
-				if (mutation.length === 0) mutation = document.querySelectorAll('.ii.gt > div > div > br + br + a')
-				const len = mutation.length
-
-				if (len === 0) return
-
-				if (mutation !== null) window.setTimeout(() => fetchFromElem(mutation), 0)
-			}
-		})
-		observer.observe(document.querySelector('div[id=":5"] + div'), {
+		observer = new MutationObserver(throttle(handleMutations, 100))
+		observer.observe(div, {
 			subtree: true,
 			childList: true,
 		})
+	}
+
+	function handleMutations(mutations) {
+		const hash = document.location.hash
+		if (!label.test(hash) && !inbox.test(hash)) return
+
+		console.log('observer')
+		for (let i = 0; i < mutations.length; ++i) {
+			let mutation = mutations[i].target.querySelector('vem')
+			if (mutation === null) mutation = mutations[i].target.querySelector('.ii.gt > div > div > br + br + a')
+			if (mutation === null) return
+
+			const len = mutation.length
+			if (len === 0) return
+			console.log(mutation)
+
+			if (mutation !== null) fetchFromElemThrottled(mutation)
+		}
+	}
+
+	function throttle(callback, wait = 100) {
+		let time
+		let lastFunc
+
+		return function throttle(...args) {
+			if (time === undefined) {
+				callback.apply(this, args)
+				time = Date.now()
+			} else {
+				clearTimeout(lastFunc)
+				lastFunc = setTimeout(() => {
+					if (Date.now() - time >= wait) {
+						callback.apply(this, args)
+						time = Date.now()
+					}
+				}, wait - (Date.now() - time))
+			}
+		}
 	}
 
 	/**
@@ -128,8 +152,10 @@
 	 */
 	function addListener() {
 		/** hashchanges */
-		window.addEventListener('DOMContentLoaded', function() {
-			if (observer === undefined) observe()
+		window.addEventListener('load', function() {
+			if (observer !== undefined) observer.disconnect()
+			observe()
+			window.setInterval(() => hrefToHash.clear(), 300000)
 		})
 	}
 
